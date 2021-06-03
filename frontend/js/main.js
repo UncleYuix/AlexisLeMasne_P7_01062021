a17c1ef5649dce4762eec1522a98ce2a264f047e
LeMasne <alexislemasne@gmail.com>
2021-06-03 14:14:04 +0200

    v-for

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,11 +3,15 @@ const app = Vue.createApp({
         return {
             product: 'puzzle',
             author: 'Jean Louis David',
+            date: "22/05/2020",
             author2: 'Jean Claude Killy',
+            author3: 'Gisèle Ruiz',
             image: 'images/puzzle.jpg',
             image2: 'images/team.jpg',
-            message1: 'magnifique, je reconnais Joel surl a photo #je suis puzzle',
-            message2: 'ahah trop rigolo xptdr'
+            message1: 'magnifique, je reconnais Joel surl a photo #je suis peeeeeeeeeeeense puzzle',
+            message2: 'ahah trop rigolo xptdr',
+            message3: "on est des déglingos",
+            gif: "https://media.giphy.com/media/Af0RkOscO3KSHZTkMl/giphy.gif"
         }
     }
 })
