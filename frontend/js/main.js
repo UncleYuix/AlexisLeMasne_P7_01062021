e603d7f9de2bb89bd6267b65cd1f410d7f56fa8e
LeMasne <alexislemasne@gmail.com>
2021-06-03 10:26:09 +0200

    frontend

---
+++ frontend/js/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'puzzle',
+            author: 'Jean Louis David',
+            author2: 'Jean Claude Killy',
+            image: 'images/puzzle.jpg',
+            image2: 'images/team.jpg',
+            message1: 'magnifique, je reconnais Joel surl a photo #je suis puzzle',
+            message2: 'ahah trop rigolo xptdr'
+        }
+    }
+})
