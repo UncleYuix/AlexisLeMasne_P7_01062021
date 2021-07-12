728cd3e7cd48331185ffdbbfc89132c777bb095f
LeMasne <alexislemasne@gmail.com>
2021-07-12 11:34:14 +0200

    Pré-Evaluation

--- front/src/components/myPublication.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<div>
-    <h1> J'affiche ici les publications postées par l'utilisateur logué </h1>
-    <p> Possibilitié lui est donné de modifié ses publications ainsi que les deletes </p>
-    </div>
-</template>
